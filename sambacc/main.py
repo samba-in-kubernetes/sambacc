eff1f7e2f8d6284b424777f12103ad6d30340f29
John Mulligan <jmulligan@redhat.com>
2020-12-09 16:55:07 -0500

    sambacc: unify env var cli handling in main function

--- sambacc/main.py
+++ sambacc/main.py
@@ -28,21 +28,51 @@ def import_config(cli, config):
 default_cfunc = print_config
 
 
-def main():
+def from_env(varname, default=None, help=None):
+    def _convert(v):
+        if not v:
+            v = os.environ.get(varname, "")
+        if not v and default is not None:
+            v = default
+        return str(v)
+
+    if help:
+        chelp = f"{help} (equivalent to setting {varname})"
+    else:
+        chelp = None
+    return {
+        "type": _convert,
+        "default": "",
+        "help": chelp,
+    }
+
+
+def main(args=None):
     parser = argparse.ArgumentParser()
-    parser.add_argument("--config", default=DEFAULT_CONFIG)
-    parser.add_argument("--identity")
+    parser.add_argument(
+        "--config",
+        **from_env(
+            "SAMBACC_CONFIG",
+            default=DEFAULT_CONFIG,
+            help="Specify source configuration",
+        ),
+    )
+    parser.add_argument(
+        "--identity",
+        **from_env(
+            "SAMBA_CONTAINER_ID",
+            help="A string identifying the local idententy",
+        ),
+    )
     sub = parser.add_subparsers()
     p_print_config = sub.add_parser("print-config")
     p_print_config.set_defaults(cfunc=print_config)
     p_import = sub.add_parser("import")
     p_import.set_defaults(cfunc=import_config)
-    cli = parser.parse_args()
+    cli = parser.parse_args(args)
 
     if not cli.identity:
-        cli.identity = os.environ.get("SAMBA_CONTAINER_ID")
-        if not cli.identity:
-            raise Fail("missing container identity")
+        raise Fail("missing container identity")
 
     cfunc = getattr(cli, "cfunc", default_cfunc)
     cfunc(cli, config)
