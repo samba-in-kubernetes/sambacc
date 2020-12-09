725b6d8dd77774c102319d304c242815e5dc7e52
John Mulligan <jmulligan@redhat.com>
2020-12-09 16:55:31 -0500

    setup.py: fix up the things check complains about

--- setup.py
+++ setup.py
@@ -3,8 +3,11 @@ from setuptools import setup, find_packages
 setup(
     name="sambacc",
     version="0.1",
-    author="John Mulligan <phlogistonjohn@asynchrono.us>",
+    author="John Mulligan",
+    author_email="phlogistonjohn@asynchrono.us",
     description="Samba Container Configurator",
+    # why does 'setup.py check' require this url field?
+    url="mailto:phlogistonjohn@asynchrono.us",
     packages=find_packages(),
     entry_points={
         "console_scripts": ["samba-container-config=sambacc.main:main"],
