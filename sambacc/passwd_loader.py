fd4f5f4481be876062bf740d65612edfd34ee0fa
John Mulligan <jmulligan@redhat.com>
2020-12-18 10:59:56 -0500

    sambacc: add rough draft passwd & group file loader
    Untested!

---
+++ sambacc/passwd_loader.py
@@ -0,0 +1,48 @@
+import os
+
+class LineFileLoader:
+    def __init__(self, path):
+        self.path = path
+        self.lines = []
+
+    def read(self):
+        with open(self.path) as f:
+            self.readfp(f)
+
+    def write(self):
+        tpath = self._tmp_path(self.path)
+        with open(tpath, "w") as f:
+            self.writefp(f)
+        os.rename(tpath, self.path)
+
+    def _tmp_path(self, path):
+        # for later: make this smarter
+        return f"{path}.tmp"
+
+    def readfp(self, fp):
+        for line in fp:
+            self.lines.append(line)
+
+    def writefp(self, fp):
+        for line in self.lines:
+            fp.write(line)
+            fp.write("\n")
+        fp.flush()
+
+
+class PasswdFileLoader(LineFileLoader):
+    def __init__(self, path="/etc/passwd"):
+        super().__init__(path)
+
+    def add_user(self, user_entry):
+        line = ":".join(user_entry.passwd_fields())
+        self.lines.apppend(line)
+
+
+class GroupFileLoader(LineFileLoader):
+    def __init__(self, path="/etc/group"):
+        super().__init__(path)
+
+    def add_user(self, user_entry):
+        line = ":".join(user_entry.passwd_fields())
+        self.lines.apppend(line)
