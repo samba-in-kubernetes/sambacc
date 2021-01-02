c247e5529c234b2d60d63c0599f8ab441f3af86e
John Mulligan <jmulligan@redhat.com>
2021-01-02 13:50:09 -0500

    sambacc: prevent adding duplicates to passwd and group files

--- sambacc/passwd_loader.py
+++ sambacc/passwd_loader.py
@@ -37,16 +37,44 @@ class LineFileLoader:
 class PasswdFileLoader(LineFileLoader):
     def __init__(self, path="/etc/passwd"):
         super().__init__(path)
+        self._usernames = set()
+
+    def readfp(self, fp):
+        super().readfp(fp)
+        self._update_usernames_cache()
+
+    def _update_usernames_cache(self):
+        for line in self.lines:
+            if ":" in line:
+                u = line.split(":")[0]
+                self._usernames.add(u)
 
     def add_user(self, user_entry):
+        if user_entry.username in self._usernames:
+            return
         line = "{}\n".format(":".join(user_entry.passwd_fields()))
         self.lines.append(line)
+        self._usernames.add(user_entry.username)
 
 
 class GroupFileLoader(LineFileLoader):
     def __init__(self, path="/etc/group"):
         super().__init__(path)
+        self._groupnames = set()
+
+    def readfp(self, fp):
+        super().readfp(fp)
+        self._update_groupnames_cache()
+
+    def _update_groupnames_cache(self):
+        for line in self.lines:
+            if ":" in line:
+                u = line.split(":")[0]
+                self._groupnames.add(u)
 
     def add_group(self, group_entry):
+        if group_entry.groupname in self._groupnames:
+            return
         line = "{}\n".format(":".join(group_entry.group_fields()))
         self.lines.append(line)
+        self._groupnames.add(group_entry.groupname)
