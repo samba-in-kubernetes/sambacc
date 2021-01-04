d5f7dbbe460ce1b2f5a3422b2d4d4dc07e34b327
John Mulligan <jmulligan@redhat.com>
2021-01-04 13:39:54 -0500

    sambacc: improve the account flag behavior in passdb loader

--- sambacc/passdb_loader.py
+++ sambacc/passdb_loader.py
@@ -1,3 +1,9 @@
+# Do the samba python bindings not export any useful constants?
+ACB_DISABLED = 0x00000001
+ACB_NORMAL = 0x00000010
+ACB_PWNOEXP = 0x00000200
+
+
 def _samba_modules():
     from samba.samba3 import param
     from samba.samba3 import passdb
@@ -29,13 +35,21 @@ class PassDBLoader:
             samu = None
         # if it doesn't exist, create it
         if samu is None:
-            # FIXME, research correct flag value
-            self._pdb.create_user(user_entry.username, 0)
+            # FIXME, research if there are better flag values to use
+            acb = ACB_NORMAL | ACB_PWNOEXP
+            self._pdb.create_user(user_entry.username, acb)
             samu = self._pdb.getsampwnam(user_entry.username)
+        acb = samu.acct_ctrl
         # update password/metadata
         if user_entry.nt_passwd:
             samu.nt_passwd = user_entry.nt_passwd
         elif user_entry.plaintext_passwd:
             samu.plaintext_passwd = user_entry.plaintext_passwd
+        # Try to mimic the behavior of smbpasswd and clear the account disabled
+        # flag when adding or updating the user.
+        # We don't expect granular, on the fly, user management in the
+        # container, so it seems pointless to have a user that can't log in.
+        if acb & ACB_DISABLED:
+            samu.acct_ctrl = acb & ~ACB_DISABLED
         # update the db
         self._pdb.update_sam_account(samu)
