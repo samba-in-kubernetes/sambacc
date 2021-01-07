96268e04c4f8201e342490aac9c2270d9e71a05f
John Mulligan <jmulligan@redhat.com>
2021-01-07 15:55:02 -0500

    container: support running passdb tests in the container env
    This adds a bunch of integration stuff to allow tox+py.test to actually
    run the passdb tests, as it needs both the samba libs and the ability
    to add matching entries to an "etc passwd" file before any interesting
    stuff happens.

--- tests/container/build.sh
+++ tests/container/build.sh
@@ -20,7 +20,18 @@ if [ "$1" ]; then
     hg update --check "$1"
 fi
 
-tox
+# Allow the tests to use customized passwd file contents in order
+# to test samba passdb support. It's a bit strange, but should work.
+# The test suite tries to restore the passwd file after changing it,
+# but you probably don't want to enable these on your desktop.
+# TODO: actually use nss-wrapper
+export WRITABLE_PASSWD=yes
+export NSS_WRAPPER_PASSWD=/etc/passwd
+export NSS_WRAPPER_GROUP=/etc/group
+
+# Run tox with sitepackages enabled to allow access to system installed samba
+# modules. The container env already provides us control over the env.
+tox --sitepackages
 
 $python setup.py sdist
 $python setup.py bdist_wheel
