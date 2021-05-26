5ff4e1f383483edcb29282381f690142e7441cda
John Mulligan <jmulligan@redhat.com>
2021-05-26 17:30:09 -0400

    sambacc: add module for file-change based waits (w/ timeout)
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

---
+++ sambacc/inotify_waiter.py
@@ -0,0 +1,83 @@
+#
+# sambacc: a samba container configuration tool
+# Copyright (C) 2021  John Mulligan
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+#
+
+import os
+
+import inotify_simple as _inotify  # type: ignore
+
+
+class INotify:
+    """A waiter that monitors a file path for changes, based on inotify.
+
+    Inotify is used to monitor the specified path for changes (writes).
+    It stops waiting when the file is changed or the timeout is reached.
+
+    A `print_func` can be specified as a simple logging method.
+    """
+
+    timeout = 300
+    print_func = None
+
+    def __init__(self, path: str, print_func=None):
+        self.print_func = print_func
+        self._inotify = _inotify.INotify()
+        dirpath, fpath = os.path.split(path)
+        if not dirpath:
+            dirpath = "."
+        if not fpath:
+            raise ValueError("a file path is required")
+        self._dir = dirpath
+        self._name = fpath
+        self._mask = _inotify.flags.DELETE | _inotify.flags.CLOSE_WRITE
+        self._inotify.add_watch(self._dir, self._mask)
+
+    def close(self) -> None:
+        self._inotify.close()
+
+    def _print(self, msg: str) -> None:
+        if self.print_func:
+            self.print_func("[inotify waiter] {}".format(msg))
+
+    def wait(self) -> None:
+        next(self._wait())
+
+    def _get_events(self):
+        timeout = 1000 * self.timeout
+        self._print("waiting {}ms for activity...".format(timeout))
+        events = self._inotify.read(timeout=timeout)
+        if not events:
+            # use "None" as a sentinel for a timeout, otherwise we can not
+            # tell if its all events that didn't match or a true timeout
+            return [None]
+        # filter out events we don't care about
+        return [
+            event
+            for event in events
+            if (event.name == self._name)
+            and ((event.mask & _inotify.flags.CLOSE_WRITE) != 0)
+        ]
+
+    def _wait(self):
+        while True:
+            for event in self._get_events():
+                if event is None:
+                    self._print("timed out")
+                    yield None
+                else:
+                    self._print(f"{self._name} modified")
+                    yield None
---
+++ tests/test_inotify_waiter.py
@@ -0,0 +1,94 @@
+#
+# sambacc: a samba container configuration tool
+# Copyright (C) 2021  John Mulligan
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+#
+
+import contextlib
+import threading
+import time
+
+import pytest
+
+try:
+    import sambacc.inotify_waiter
+except ImportError:
+    pytestmark = pytest.mark.skip
+
+
+@contextlib.contextmanager
+def background(bg_func):
+    t = threading.Thread(target=bg_func)
+    t.start()
+    try:
+        yield None
+    finally:
+        t.join()
+
+
+def test_inotify(tmp_path):
+    tfile = str(tmp_path / "foobar.txt")
+    tfile2 = str(tmp_path / "other.txt")
+
+    iw = sambacc.inotify_waiter.INotify(tfile, print_func=print)
+    iw.timeout = 3
+
+    def _touch():
+        time.sleep(0.2)
+        with open(tfile, "w") as fh:
+            print("W", tfile)
+            fh.write("one")
+
+    with background(_touch):
+        before = time.time()
+        iw.wait()
+        after = time.time()
+    assert after - before > 0.1
+    assert after - before <= 1
+
+    def _touch2():
+        time.sleep(0.2)
+        with open(tfile2, "w") as fh:
+            print("W", tfile2)
+            fh.write("two")
+        time.sleep(1)
+        with open(tfile, "w") as fh:
+            print("W", tfile)
+            fh.write("one")
+
+    with background(_touch2):
+        before = time.time()
+        iw.wait()
+        after = time.time()
+
+    assert after - before > 0.1
+    assert after - before >= 1
+
+    before = time.time()
+    iw.wait()
+    after = time.time()
+    assert int(after) - int(before) == 3
+    iw.close()
+
+
+def test_inotify_bad_input():
+    with pytest.raises(ValueError):
+        sambacc.inotify_waiter.INotify("/")
+
+
+def test_inotify_relative_path():
+    iw = sambacc.inotify_waiter.INotify("cool.txt")
+    assert iw._dir == "."
+    assert iw._name == "cool.txt"
