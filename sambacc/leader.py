4a9532c41a93b5a5be5bc93824dcd79fd2919b8d
John Mulligan <jmulligan@redhat.com>
2021-11-03 14:19:10 -0400

    sambacc: add a new protocol type for cluster leader location
    Add a minimal protocol type that helps us determine (for now) or
    acquire (in the future, maybe) cluster leader status.
    This will be used to perform certain actions only on the leader "node".
    
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

---
+++ sambacc/leader.py
@@ -0,0 +1,39 @@
+#
+# sambacc: a samba container configuration tool
+# Copyright (C) 2021  John Mulligan
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+#
+
+import typing
+
+
+class LeaderStatus(typing.Protocol):
+    """Fetches information about the current cluster leader."""
+
+    def is_leader(self) -> bool:
+        ...
+
+
+class LeaderLocator(typing.Protocol):
+    """Acquire state needed to determine or fix a cluster leader.
+    Can be used for purely informational types or types that
+    actually acquire cluster leadership if needed.
+    """
+
+    def __enter__(self) -> LeaderStatus:
+        ...
+
+    def __exit__(self, exc_type, exc_val, exc_tb) -> None:
+        ...
