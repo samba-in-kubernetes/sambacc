6bc7d67c7d1ce8033d257a50317e7573c6d75b89
John Mulligan <jmulligan@redhat.com>
2022-05-03 16:51:58 -0400

    sambacc: add a small file for non-sambacc specific type annotations
    These types are "consumed" by the lib but annoying to remember
    and type (with a keyboard) over and over again.
    
    
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

---
+++ sambacc/typelets.py
@@ -0,0 +1,27 @@
+#
+# sambacc: a samba container configuration tool
+# Copyright (C) 2022  John Mulligan
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+#
+"""typelets defines common-ish type hinting types that are tedious to
+remember/redefine.
+"""
+
+from types import TracebackType
+import typing
+
+ExcType = typing.Optional[typing.Type[BaseException]]
+ExcValue = typing.Optional[BaseException]
+ExcTraceback = typing.Optional[TracebackType]
