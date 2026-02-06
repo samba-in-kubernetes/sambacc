3057b9a6bc0e62ad0cd624741828bfb17172e871
John Mulligan <jmulligan@redhat.com>
2026-02-06 14:52:41 -0500

    grpc: add config.py to establish configuration focused types
    Add new set of configuration focused types that will eventually
    replace the config type in server.py. This is in preparation
    for supporting more than one listening port.
    
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

---
+++ sambacc/grpc/config.py
@@ -0,0 +1,73 @@
+#
+# sambacc: a samba container configuration tool (and more)
+# Copyright (C) 2026  John Mulligan
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+#
+
+from typing import Optional
+
+import dataclasses
+import enum
+
+from ..typelets import Self
+
+
+class ClientVerification(str, enum.Enum):
+    INSECURE = "insecure"
+    TLS = "tls"
+
+
+@dataclasses.dataclass
+class ConnectionConfig:
+    address: str = "localhost:54445"
+    verification: ClientVerification = ClientVerification.INSECURE
+    server_key: Optional[bytes] = None
+    server_cert: Optional[bytes] = None
+    ca_cert: Optional[bytes] = None
+
+    def describe(self) -> str:
+        _kind = "tcp socket"
+        if self.address.startswith("unix:"):
+            _kind = "unix socket"
+        return f"{_kind}/{self.verification.value}"
+
+    # compat properties
+    @property
+    def insecure(self) -> bool:
+        return self.verification is ClientVerification.INSECURE
+
+    @insecure.setter
+    def insecure(self, value: bool) -> None:
+        self.verification = (
+            ClientVerification.INSECURE if value else ClientVerification.TLS
+        )
+
+
+@dataclasses.dataclass
+class ServerConfig:
+    max_workers: int
+    read_only: bool
+    connections: list[ConnectionConfig]
+
+    @classmethod
+    def default(cls) -> Self:
+        return cls(
+            max_workers=8,
+            read_only=False,
+            connections=[ConnectionConfig()],
+        )
+
+    def first_connection(self) -> ConnectionConfig:
+        return self.connections[0]
